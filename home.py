cda8fa3b5af21eb87d8961ef59c7e3e987b52f23
yeon <149470171+HurYeonhoo@users.noreply.github.com>
2024-11-15 00:34:57 +0900

    Update home.py

--- home.py
+++ home.py
@@ -7,7 +7,6 @@ from utils import get_text, get_text_chunks, chunk_dataframe_to_documents, get_v
 from review_crawling import Crawling
 from review_classification import Classification
 import os
-from chatbot_gradio import save_chatbot_config, launch_chatbot
 
 # OpenAI API 키 설정
 openai_api_key = st.secrets["OPENAI_API_KEY"]
